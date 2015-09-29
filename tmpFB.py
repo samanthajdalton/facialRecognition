aac0b137f0409b572e0ea2fabe0d6daf02d88a4f
samanthajdalton <samanthajdalton@gmail.com>
2015-09-29 18:58:35 +0200

    vm code

--- tmpFB.py
+++ tmpFB.py
@@ -18,7 +18,12 @@ conn = pymongo.MongoClient()
 db = conn.faces
 collection = db.urlClubPhotos
 
+#Set toScroll = False for testing & fbPageName is name of club's fb page
+listOfUrls = []
+listOfImageUrls = []
+
 def getPicUrls(fbPageName, toScroll):
+    pg = 0
     driver = webdriver.Chrome()
     driver.get('https://www.facebook.com/' + fbPageName + '/photos_stream?')
 
@@ -26,34 +31,32 @@ def getPicUrls(fbPageName, toScroll):
     r = requests.get(url)
     facebookID = r.text.split('"pageID":"')[1].split('"')[0]
        
-   
-    db.drop_collection('temp')
-    db.create_collection('temp')
     #Get thumbnails of images    
     scroll = True
     while scroll:
+        pg += 1
+        print(pg)
         a = len(listOfUrls)
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         for i in driver.find_elements_by_xpath("//*[@class='uiMediaThumb _6i9 uiMediaThumbMedium']"):
             tempDict = {}
             tempDict['thumbUrl'] = i.get_attribute("href")
-            r = requests.get(thumbUrl)
+            listOfUrls.append(tempDict['thumbUrl'])
+            r = requests.get(tempDict['thumbUrl'])
             tree = html.fromstring(r.text)
             for htmlObject in tree.xpath("//img[@class='fbPhotoImage img']"): 
                 tempDict['urlOfImage'] = htmlObject.attrib['src']
-                listOfImageUrls.append(urlOfImage)
+                listOfImageUrls.append(tempDict['urlOfImage'])
             for htmlObject2 in tree.xpath("//abbr"):  
                 tempDict['albumTitle'] = htmlObject2.attrib['title']
                 tempDict['uploadTime'] = htmlObject2.attrib['data-utime']
                 tempDict['fbPageName'] = fbPageName
             collection.insert(tempDict)
             
-            scroll = toScroll
+        scroll = toScroll
 
         b = len(listOfUrls)
 
         
         if a == b:
             scroll = False
-
-#getPicUrls('opiumbarcelona', False)
